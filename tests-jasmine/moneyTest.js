b631d8beb527b05d3c129dedbc989cb6bd118c53
filiptota <filip.tota5@gmail.com>
2024-11-22 22:09:30 +0100

    remove default tests and add money tests, using jasmine functions (describe, it, expect)

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,30 @@
+import { formatCurrency } from "../js/utils/money.js";
+
+// create test suite
+describe("test suite: formatCurrency", () => {
+  // to create a test in jasmine we will use it() function
+  it("converts cents into dollars", () => {
+    expect(formatCurrency(2095)).toEqual("20.95");
+    // expect let's us compare one value to other value
+    // expect gives us an object and this object has many methods we can use to do comparisons
+    // one of the method is .toEqual() which takes a value to compare
+  });
+
+  it("works with zero", () => {
+    expect(formatCurrency(0)).toEqual("0.00");
+  });
+
+  describe("rounding", () => {
+    it("rounds up to the nearest cent", () => {
+      expect(formatCurrency(2000.5)).toEqual("20.01");
+    });
+
+    it("rounds down to the neartest cent", () => {
+      expect(formatCurrency(2000.4)).toEqual("20.00");
+    });
+  });
+}); // function provided by jasmine and it creates a group of specs/tests (often called test suite)
+// forst parameter is a name for test
+// second parameter is function that adds tests
+
+// we can use describe inside describe --- example above
